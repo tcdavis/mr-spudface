73b01c6096934132bde61e4b689b04ece6917794
TC Davis <thomas.c.davis@gmail.com>
2019-04-02 22:58:22 -0700

    add more gesture logic, hit bug in my animated code

--- components/PotatoHead.js
+++ components/PotatoHead.js
@@ -4,14 +4,51 @@ import { StyleSheet, Text, View, Dimensions } from "react-native";
 import Animated from "react-native-reanimated";
 import { PanGestureHandler, State } from "react-native-gesture-handler";
 
+const { cond, eq, add, set, Value, event } = Animated;
 const { width, height } = Dimensions.get("window");
 
 export class PotatoHead extends React.Component {
   render() {
+    dragX = new Value(0);
+    dragY = new Value(0);
+    offsetX = new Value(width / 2);
+    offsetY = new Value(height / 2);
+
+    gestureState = new Value(-1);
+    onGestureEvent = event([
+      {
+        nativeEvent: {
+          translationX: this.dragX,
+          translationY: this.dragY,
+          state: this.gestureState,
+        },
+      },
+    ]);
+    // there is a bug here: undefined is not an object
+
+    /* transX = cond(
+     *   eq(this.gestureState, State.ACTIVE),
+     *   add(this.offsetX, this.dragX),
+     *   set(this.offsetX, add(this.offsetX, this.dragX)),
+     * );
+     * transY = cond(
+     *   eq(this.gestureState, State.ACTIVE),
+     *   add(this.offsetY, this.dragY),
+     *   set(this.offsetY, add(this.offsetY, this.dragY)),
+     * );
+     */
+
+    /* { transform: [
+     *     { translateX: this.transX },
+     *     { translateY: this.transY },
+     * ] }
+     */
     return (
       <View style={styles.container}>
         <PanGestureHandler
           maxPointers={1}
+          onGestureEvent={this.onGestureEvent}
+          onHandlerStateChange={this.onGestureEvent}
         >
           <Animated.View
             style={[
