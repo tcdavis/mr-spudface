e18fcc96dad81d4a0c048bb54d6df7c7e0f616ec
TC Davis <thomas.c.davis@gmail.com>
2019-04-02 23:46:39 -0700

    extract DraggableElement component

---
+++ components/DraggableElement.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+
+import Animated from "react-native-reanimated";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+
+const { cond, eq, add, set, Value, event } = Animated;
+
+export default class DraggableElement extends React.Component {
+    dragX = new Value(0);
+    dragY = new Value(0);
+    offsetX = new Value(this.props.pos.x);
+    offsetY = new Value(this.props.pos.y);
+
+    gestureState = new Value(-1);
+    onGestureEvent = event([
+      {
+        nativeEvent: {
+          translationX: this.dragX,
+          translationY: this.dragY,
+          state: this.gestureState,
+        },
+      },
+    ]);
+
+    transX = cond(
+      eq(this.gestureState, State.ACTIVE),
+
+      add(this.offsetX, this.dragX),
+      set(this.offsetX, add(this.offsetX, this.dragX)),
+    );
+    transY = cond(
+      eq(this.gestureState, State.ACTIVE),
+
+      add(this.offsetY, this.dragY),
+      set(this.offsetY, add(this.offsetY, this.dragY)),
+    );
+
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <PanGestureHandler
+          maxPointers={1}
+          onGestureEvent={this.onGestureEvent}
+          onHandlerStateChange={this.onGestureEvent}
+        >
+          <Animated.View
+            style={[
+              this.props.style,
+              {
+                transform: [
+                  {translateX: this.transX},
+                  {translateY: this.transY},
+                ],
+              },
+            ]}
+          />
+        </PanGestureHandler>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
--- components/PotatoHead.js
+++ components/PotatoHead.js
@@ -1,64 +1,15 @@
 import React from "react";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
+import DraggableElement from './DraggableElement'
 
-import Animated from "react-native-reanimated";
-import { PanGestureHandler, State } from "react-native-gesture-handler";
-
-const { cond, eq, add, set, Value, event } = Animated;
 const { width, height } = Dimensions.get("window");
 
 export class PotatoHead extends React.Component {
-    dragX = new Value(0);
-    dragY = new Value(0);
-    offsetX = new Value(width / 2);
-    offsetY = new Value(height / 2);
-
-    gestureState = new Value(-1);
-    onGestureEvent = event([
-      {
-        nativeEvent: {
-          translationX: this.dragX,
-          translationY: this.dragY,
-          state: this.gestureState,
-        },
-      },
-    ]);
-
-    transX = cond(
-      eq(this.gestureState, State.ACTIVE),
-
-      add(this.offsetX, this.dragX),
-      set(this.offsetX, add(this.offsetX, this.dragX)),
-    );
-    transY = cond(
-      eq(this.gestureState, State.ACTIVE),
-
-      add(this.offsetY, this.dragY),
-      set(this.offsetY, add(this.offsetY, this.dragY)),
-    );
-
-
   render() {
-
     return (
       <View style={styles.container}>
-        <PanGestureHandler
-          maxPointers={1}
-          onGestureEvent={this.onGestureEvent}
-          onHandlerStateChange={this.onGestureEvent}
-        >
-          <Animated.View
-            style={[
-              styles.box,
-              {
-                transform: [
-                  {translateX: this.transX},
-                  {translateY: this.transY},
-                ],
-              },
-            ]}
-          />
-        </PanGestureHandler>
+        <DraggableElement style={styles.car} pos={{x: width / 3, y: height / 3}}/>
+        <DraggableElement style={styles.box} pos={{x: 2 * width / 3, y: 2 * height / 3}}/>
       </View>
     );
   }
